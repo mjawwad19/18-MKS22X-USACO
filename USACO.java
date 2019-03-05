1359ad889dde9b58ced2e8051c33966eb663827e
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-05 17:45:03 -0500

    added skeleton of fxns

---
+++ USACO.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class USACO{
+  public static int bronze(String fileName){
+    return -1;
+  }
+  public static int silver(String fileName) {
+    return -1;
+  }
+}
