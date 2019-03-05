2aed0fe8cab5c075a21341018bf072dfa42cdb58
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-05 18:01:42 -0500

    converted the land that will be leveled into an int[][] and tested proper conversion

--- USACO.java
+++ USACO.java
@@ -4,9 +4,29 @@ public class USACO{
   public static int bronze(String fileName) throws FileNotFoundException{
     File text = new File(fileName);
     Scanner inf = new Scanner(text);
+    String[] lvls = inf.nextLine().split(" ");
+    int[][] grid = new int[Integer.parseInt(lvls[0])][Integer.parseInt(lvls[1])];
+    //the first two terms listed are R and C anyway so.
+    for (int r = 0; r < grid.length; r++) {
+      String[] row = inf.nextLine().split(" ");
+      for (int c = 0; c < grid[0].length; c++) {
+        grid[r][c] = Integer.parseInt(row[c]);
+      }
+      int rowNum = r + 1;
+      System.out.print("row " + rowNum + ": " + Arrays.toString(grid[r]) + "\n");
+    }
+    //System.out.println(Arrays.toString(grid));
     return -1;
   }
   public static int silver(String fileName) {
     return -1;
+  }
+
+  public static void main(String[] args) {
+    try {
+      bronze("makelake.1.in");
+    }catch (FileNotFoundException e) {
+      System.out.println("enter a file known");
+    }
   }
 }
