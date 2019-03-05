b5db7cdee80b5f06b14fc15df685c0a95d8ec932
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-05 17:47:50 -0500

    now reads whatever input, time to download the inputs and see how they are formatted

--- USACO.java
+++ USACO.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
-  public static int bronze(String fileName){
+  public static int bronze(String fileName) throws FileNotFoundException{
+    File text = new File(fileName);
+    Scanner inf = new Scanner(text);
     return -1;
   }
   public static int silver(String fileName) {
